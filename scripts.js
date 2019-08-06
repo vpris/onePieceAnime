0818038bc18bdea0f1dcd875766a103fd32839a7
Vladimir <vpris@inbox.ru>
2019-08-06 19:34:10 +0600

    1

---
+++ scripts.js
@@ -0,0 +1,50 @@
+"use strict";
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    video: true,
+
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+let removePrev = document.querySelector('.owl-prev');
+removePrev.style.display = 'none';
+
+let stylingNext = document.querySelector('.owl-next');
+stylingNext.style.position = 'absolute';
+stylingNext.style.top = '-5px';
+stylingNext.style.right = '-5px';
+stylingNext.style.height = '300px';
+stylingNext.style.width = '80px';
+stylingNext.style.color = '#ffffff';
+stylingNext.style.backgroundColor = '#918f90';
+stylingNext.style.fontSize = '6em';
+stylingNext.style.borderRadius = '0';
+
+
+
+
+echo "# 11" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/vpris/11.git
+git push -u origin master
