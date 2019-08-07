366e517add46f90896f21fd8f8032e83305d3e60
Vladimir <vpris@inbox.ru>
2019-08-07 18:04:40 +0600

    New editing on menu

--- scripts.js
+++ scripts.js
@@ -38,3 +38,7 @@ stylingNext.style.color = '#ffffff';
 stylingNext.style.backgroundColor = '#918f90';
 stylingNext.style.fontSize = '6em';
 stylingNext.style.borderRadius = '0';
+
+
+if ($(document).height() <= $(window).height())
+    $("footer.footer").addClass("navbar-fixed-bottom");
