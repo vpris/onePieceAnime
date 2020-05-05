fd99f90601ff91c762f50418741d2beafe589a78
vprisyazhnikov <vprisyazhnikov@utrace.ru>
2020-05-05 07:18:59 +0600

    Added

--- assets/js/staff_sheet.js
+++ assets/js/staff_sheet.js
@@ -1,5 +1,3 @@
-
-
 (function () {
     var app = "https://script.googleusercontent.com/macros/echo?user_content_key=AY0nubBrO6fxv2C7JA0A0WO1ydcbtBUnQTbEZ9rJ_otrJXm6XTZT5-u0a5dgfBtPSk37Yndu-HKIydS9iP_HRe6AWqIWtBclm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDv9fMtrRuWYqTGRgrGwqxu8pt0AHbCtYRjpOW3H9u0qV9kM18FOjZlN2Dzp8AowX-IqWsDnSrEd&lib=MTm1d2VhxMXdkI-R0C0RtPJZBpkQJSAEH",
        output = '',
