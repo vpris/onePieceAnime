d54f6e0749de4046681234a843b1144206638b6d
vprisyazhnikov <vprisyazhnikov@utrace.ru>
2020-05-05 08:46:08 +0600

    Added 1

--- assets/js/staff_sheet.js
+++ assets/js/staff_sheet.js
@@ -1,5 +1,5 @@
 (function () {
-    var app = "https://script.googleusercontent.com/macros/echo?user_content_key=AY0nubBrO6fxv2C7JA0A0WO1ydcbtBUnQTbEZ9rJ_otrJXm6XTZT5-u0a5dgfBtPSk37Yndu-HKIydS9iP_HRe6AWqIWtBclm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDv9fMtrRuWYqTGRgrGwqxu8pt0AHbCtYRjpOW3H9u0qV9kM18FOjZlN2Dzp8AowX-IqWsDnSrEd&lib=MTm1d2VhxMXdkI-R0C0RtPJZBpkQJSAEH",
+    var app = "https://script.google.com/macros/s/AKfycbxAVukZVJT6563Ay29UPGAyo5FGDI9Ds4uigdz7Xs7egofrlgY/exec",
        output = '',
        xhr = new XMLHttpRequest();
     xhr.open('GET', app);
@@ -11,7 +11,8 @@
                 result = r["name"];
              for (var i = 0; i < result.length; i++){
                    var obj = 
-                    '<div class="staffName col-md-6">' + 
+                   '<img class="staffUserpic col-md-1" src="' + r["imageLink"][i] + '">' +
+                    '<div class="staffName col-md-5">' + 
                             r["name"][i] + 
                     '</div>' +
                     '<div class="staffPosition col-md-6">' +
